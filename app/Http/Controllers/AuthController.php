ec63b66201523ce27b69d645031eb17fb3f937ba
KaliGa70 <1andresga07@gmail.com>
2024-08-13 09:48:07 -0600

    x

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,6 @@ class AuthController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
-
             ]);
 
             // Generar el token
