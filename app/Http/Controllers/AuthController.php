fe7b3d62cc854f82d2cbe54b2b54f27a5d0874b9
albertoKanroji <albert.hdz855@gmail.com>
2024-08-13 09:41:50 -0600

    x

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
             $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
-                'password' => 'required|string|min:8|confirmed',
+                'password' => 'required|string|min:8',
             ]);
 
             // Crear el usuario
