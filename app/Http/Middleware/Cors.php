4c76915bb27fa7cd63baafe1883e10f7fc9e1bb7
KaliGa70 <1andresga07@gmail.com>
2024-08-19 18:38:02 -0600

    desde el server x2

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
